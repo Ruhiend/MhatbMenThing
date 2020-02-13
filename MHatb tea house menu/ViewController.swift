c920cb82f12cdb774af661662ea9b294233df132
Ruhiend <vallmarco02@gmail.com>
2020-02-13 08:22:14 -0500

    Initial Commit

---
+++ MHatb tea house menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MHatb tea house menu
+//
+//  Created by Marco Vallinino (student HH) on 2/13/20.
+//  Copyright © 2020 Vallinino, Marco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MHatb tea house menuTests/MHatb_tea_house_menuTests.swift
@@ -0,0 +1,34 @@
+//
+//  MHatb_tea_house_menuTests.swift
+//  MHatb tea house menuTests
+//
+//  Created by Marco Vallinino (student HH) on 2/13/20.
+//  Copyright © 2020 Vallinino, Marco. All rights reserved.
+//
+
+import XCTest
+@testable import MHatb_tea_house_menu
+
+class MHatb_tea_house_menuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
